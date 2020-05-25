c14c340512d7788c38df5e66332353e1197b7e8c
SoyDiego <diegofranchina@gmail.com>
2020-05-25 19:51:31 +0200

    Finished project

--- breakingbad/src/App.jsx
+++ breakingbad/src/App.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import Frase from './components/Frase';
 import styled from '@emotion/styled'
 
 const Contenedor = styled.div`
@@ -28,10 +29,30 @@ const Boton = styled.button`
 		background-size: 400px;
 	}
 `;
+
 function App() {
+
+  const [frase, guardarFrase] = useState({})
+
+  const consultarAPI = async() => {
+    const api = await fetch('https://breaking-bad-quotes.herokuapp.com/v1/quotes')
+    const frase = await api.json()
+    guardarFrase(frase[0])
+  }
+
+  useEffect(() =>{
+    consultarAPI()
+  }, [])
+
   return (
     <Contenedor>
-      <Boton >Obtener Frase</Boton>
+      <Frase 
+        frase={frase}
+      />
+
+      <Boton 
+        onClick={consultarAPI}
+      >Obtener Frase</Boton>
     </Contenedor>
   );
 }
