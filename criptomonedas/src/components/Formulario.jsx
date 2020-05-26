09ed09de5fc3486590832273057f8625e904febc
SoyDiego <diegofranchina@gmail.com>
2020-05-26 23:51:41 +0200

    Implemented custom hooks and api call

--- criptomonedas/src/components/Formulario.jsx
+++ criptomonedas/src/components/Formulario.jsx
@@ -1,42 +1,64 @@
-import React from 'react'
+import React, {useState, useEffect} from "react";
 import styled from "@emotion/styled";
+import axios from 'axios'
 
-import useMoneda from '../hooks/useMoneda'
+//Hooks personalizados
+import useMoneda from "../hooks/useMoneda";
+import useCriptomoneda from "../hooks/useCriptomoneda";
 
 const Boton = styled.input`
-    margin-top: 20px;
-    font-weight: bold;
-    font-size: 20px;
-    padding: 10px;
-    background-color: #66a2fe;
-    border: none;
-    width: 100%;
-    border-radius: 10px;
-    color: #FFFFFF;
-    transition: background-color .3s ease;
+	margin-top: 20px;
+	font-weight: bold;
+	font-size: 20px;
+	padding: 10px;
+	background-color: #66a2fe;
+	border: none;
+	width: 100%;
+	border-radius: 10px;
+	color: #ffffff;
+	transition: background-color 0.3s ease;
 
-    &:hover{
-        background-color: #326ac0;
-        cursor: pointer;
-    }
-`
+	&:hover {
+		background-color: #326ac0;
+		cursor: pointer;
+	}
+`;
 
 const Formulario = () => {
 
-    //Utilizando useMoneda (nuestro custom hook creado)
-    const [moneda, SelectMonedas, actualizarState] = useMoneda(); 
+    //State del listado de criptomonedas
+    const [listacripto, guardarCriptomonedas] = useState([])
 
-    return (
-        <form>
+	const MONEDAS = [
+		{ codigo: "USD", nombre: "Dolar de Estados Unidos" },
+		{ codigo: "ARS", nombre: "Peso Argentino" },
+		{ codigo: "EUR", nombre: "Euro" },
+		{ codigo: "GBP", nombre: "Libra Esterlina" },
+	];
 
-            <SelectMonedas />
+	//Utilizando useMoneda 
+    const [moneda, SelectMonedas] = useMoneda("Elige tu moneda","",MONEDAS);
+    
+    //Utilizando useCriptomoneda
+    const [criptomoneda, SelectCripto] = useCriptomoneda('Elige tu Criptomoneda','', listacripto)
 
-            <Boton
-                type="submit"
-                value="Calcular"
-            />
-        </form>
-    )
-}
+    useEffect(() =>{
+        const consultarAPI = async () =>{
+            const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD'
+            const resultado = await axios.get(url);
+            guardarCriptomonedas(resultado.data.Data)
+        }
+        consultarAPI();
+    }, [])
 
-export default Formulario
+	return (
+		<form>
+			<SelectMonedas />
+            <SelectCripto />
+
+			<Boton type="submit" value="Calcular" />
+		</form>
+	);
+};
+
+export default Formulario;
--- criptomonedas/src/hooks/useMoneda.jsx
+++ criptomonedas/src/hooks/useMoneda.jsx
@@ -1,22 +1,49 @@
 import React, {Fragment, useState} from 'react'
+import styled from '@emotion/styled';
 
-const useMoneda = () => {
+const Label = styled.label`
+    font-family: 'Bebas Neue', cursive;
+    color: #FFFFFF;
+    text-transform: uppercase;
+    font-weight: bold;
+    font-size: 2.4 rem;
+    margin-top: 2rem;
+    display: block;
+`
+
+const Select = styled.select`
+    width: 100%;
+    display: block;
+    padding: 1rem;
+    -webkit-appearance: none;
+    border-radius: 10px;
+    border: none;
+    font-size: 1.2rem;
+`
+
+const useMoneda = (label, stateInicial, opciones) => {
 
     //State de nuestro custom hook
-    const [state, actualizarState] = useState('')
+    const [state, actualizarState] = useState(stateInicial)
 
-    const Seleccionar = () => (
+    const SelectMoneda = () => (
         <Fragment>
-            <label>Moneda</label>
-            <select>
-                <option value="MXN">Peso Mexicano</option>
-            </select>
+            <Label>{label}</Label>
+            <Select
+                onChange={e => actualizarState(e.target.value)}
+                value={state}
+            >
+                <option value="">--Seleccione--</option>
+                {opciones.map(opcion =>
+                    <option key={opcion.codigo} value={opcion.codigo}>{opcion.nombre}</option>
+                )}
+            </Select>
         </Fragment>
     )
 
     //Retornar state, interfaz y funcion q modifica el state.
 
-    return [state, Seleccionar, actualizarState];
+    return [state, SelectMoneda, actualizarState];
 }
 
 export default useMoneda
