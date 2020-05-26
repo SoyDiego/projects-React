7823fc0c0d95c81f05779516c7f1533c94647337
SoyDiego <diegofranchina@gmail.com>
2020-05-27 01:19:14 +0200

    Added Spinner

--- criptomonedas/src/App.jsx
+++ criptomonedas/src/App.jsx
@@ -5,6 +5,7 @@ import axios from "axios";
 import imagen from "./cryptomonedas.png";
 import Formulario from "./components/Formulario";
 import Cotizacion from "./components/Cotizacion";
+import Spinner from "./components/Spinner"
 
 const Contenedor = styled.div`
 	max-width: 900px;
@@ -41,7 +42,8 @@ const Heading = styled.h1`
 function App() {
 	const [moneda, guardarMoneda] = useState("");
 	const [criptomoneda, guardarCriptomoneda] = useState("");
-	const [resultado, guardarResultado] = useState({})
+	const [resultado, guardarResultado] = useState({});
+	const [cargando, guardarCargando] = useState(false);
 
 	useEffect(() => {
 		(async () => {
@@ -51,10 +53,19 @@ function App() {
 			//Consultar API
 			const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
 			const resultado = await axios.get(url);
-			guardarResultado(resultado.data.DISPLAY[criptomoneda][moneda])
-			
+
+			//Mostrar spinner
+			guardarCargando(true);
+
+			setTimeout(() => {
+				guardarCargando(false);
+				guardarResultado(resultado.data.DISPLAY[criptomoneda][moneda]);
+			}, 2000);
+
 		})();
 	}, [moneda, criptomoneda]);
+
+	const componente = cargando ? <Spinner /> : <Cotizacion resultado={resultado} />
 
 	return (
 		<Contenedor>
@@ -68,9 +79,7 @@ function App() {
 					guardarCriptomoneda={guardarCriptomoneda}
 				/>
 
-				<Cotizacion 
-					resultado={resultado}
-				/>
+				{componente}
 			</div>
 		</Contenedor>
 	);
