92499bac79c2890d951d37d2c533d12058632f6f
SoyDiego <diegofranchina@gmail.com>
2020-05-28 23:55:54 +0200

    Done ListadoImagenes and Imagen

--- pixabay/src/App.jsx
+++ pixabay/src/App.jsx
@@ -1,20 +1,37 @@
-import React, {useState} from 'react';
-import Formulario from './Components/Formulario'
+import React, { useState, useEffect } from "react";
+import Formulario from "./Components/Formulario";
+import ListadoImagenes from "./Components/ListadoImagenes";
 
 function App() {
+  const [busqueda, guardarBusqueda] = useState("");
+  const [imagenes, guardarImagenes] = useState([]);
 
-  const [busqueda, guardarBusqueda] = useState('')
+	useEffect(() => {
+		if (busqueda === "") return;
 
-  return (
-    <div className="container">
-      <div className="jumbotron">
-        <p className="lead text-center">
-          Buscador de Imagenes
-        </p>
-        <Formulario guardarBusqueda={guardarBusqueda} />
+		(async () => {
+			const imagenesPorPagina = 30;
+      const key = process.env.REACT_APP_API_KEY;
+			const url = `https://pixabay.com/api/?key=${key}&q=${busqueda}&per_page=${imagenesPorPagina}`;
+
+			const respuesta = await fetch(url);
+			const resultado = await respuesta.json();
+
+			guardarImagenes(resultado.hits);
+		})();
+	}, [busqueda]);
+
+	return (
+		<div className="container">
+			<div className="jumbotron">
+				<p className="lead text-center">Buscador de Imagenes</p>
+				<Formulario guardarBusqueda={guardarBusqueda} />
+			</div>
+      <div className="row justify-content-center">
+        <ListadoImagenes imagenes={imagenes}/>
       </div>
-    </div>
-  );
+		</div>
+	);
 }
 
 export default App;
