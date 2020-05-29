35d821c30be1632afe9b1aded7ffd30b84395f26
SoyDiego <diegofranchina@gmail.com>
2020-05-29 23:40:07 +0200

    Added context Categorias

--- bebidas/src/components/Formulario.jsx
+++ bebidas/src/components/Formulario.jsx
@@ -1,10 +1,43 @@
-import React from 'react'
-
+import React, {useContext} from 'react'
+import {CategoriasContext} from '../context/CategoriasContext'
 const Formulario = () => {
+
+    const { categorias } = useContext(CategoriasContext);
+    
+    console.log(categorias);
     return (
-        <div>
-            <h1>FOrmulario aqui</h1>
-        </div>
+        <form className="col-12">
+            <fieldset className="text-center">
+                <legend>Busca bebidas por Categoría o Ingrediente</legend>
+            </fieldset>
+
+            <div className="row mt-4">
+                <div className="col-md-4">
+                    <input 
+                        type="text"
+                        className="form-control"
+                        name="nombre"
+                        placeholder="Buscar por Ingrediente"
+                    />
+                </div>
+                <div className="col-md-4">
+                    <select
+                        className="form-control" 
+                        name="categoria"
+                    >
+                        <option>--Selecciona Categoría--</option>
+                    </select>
+                </div>
+
+                <div className="col-md-4">
+                    <input 
+                        type="submit"
+                        className="btn btn-block btn-primary"
+                        value="Buscar Bebidas"
+                    />
+                </div>
+            </div>
+        </form>
     )
 }
 
