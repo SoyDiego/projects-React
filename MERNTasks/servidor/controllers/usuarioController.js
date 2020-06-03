87b98839862d44ffc4fb037c3c25c188d0df1604
SoyDiego <diegofranchina@gmail.com>
2020-06-03 04:33:24 +0200

    Added JSON WebToken

--- MERNTasks/servidor/controllers/usuarioController.js
+++ MERNTasks/servidor/controllers/usuarioController.js
@@ -1,21 +1,21 @@
 const Usuario = require("../models/Usuario");
-const bcryptjs = require('bcrypt')
-const { validationResult } = require('express-validator')
+const bcryptjs = require("bcrypt");
+const { validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
 
 exports.crearUsuario = async (req, res) => {
+	//Revisar si hay errores
+	const errores = validationResult(req);
 
-    //Revisar si hay errores
-    const errores = validationResult(req)
-
-    if (!errores.isEmpty()) {
-        return res.status(400).json({errores: errores.array()})
-    }
+	if (!errores.isEmpty()) {
+		return res.status(400).json({ errores: errores.array() });
+	}
 
 	//extraer email y password
 	const { email, password } = req.body;
 
 	try {
-        //Revisa que el usuario de la BD sea único
+		//Revisa que el usuario de la BD sea único
 		let usuario = await Usuario.findOne({ email });
 
 		if (usuario) {
@@ -25,15 +25,35 @@ exports.crearUsuario = async (req, res) => {
 		//crea el nuevo usuario
 		usuario = new Usuario(req.body);
 
-        //Hashear el passowrd
-        const salt = await bcryptjs.genSalt(10)
-        usuario.password = await bcryptjs.hash(password, salt)
+		//Hashear el passowrd
+		const salt = await bcryptjs.genSalt(10);
+		usuario.password = await bcryptjs.hash(password, salt);
 
 		//guardar el usuario
 		await usuario.save();
 
-		//Mensaje de confirmación
-		res.json({ msg: "Usuario agregado correctamente" });
+		//Crear y firmar el JWT
+		const payload = {
+            usuario: {
+                id: usuario.id
+            }
+        };
+
+		//Firmar token
+		jwt.sign(
+			payload,
+			process.env.SECRETA,
+			{
+				expiresIn: 3600,
+			},(error, token) => {
+				if (error) throw error;
+
+				//Mensaje de confirmación
+				res.json({
+					token,
+				});
+			}
+		);
 	} catch (error) {
 		console.log(error);
 		res.status(400).send("Hubo un error");
