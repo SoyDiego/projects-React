23101281992f063dda61d13a2e68350519f95dc8
SoyDiego <diegofranchina@gmail.com>
2020-06-04 05:22:28 +0200

    Get and Update Tareas done

--- MERNTasks/servidor/controllers/tareaController.js
+++ MERNTasks/servidor/controllers/tareaController.js
@@ -7,6 +7,113 @@ exports.crearTarea = async (req, res) => {
 	const errores = validationResult(req);
 
 	if (!errores.isEmpty()) {
-		return res.status(400).json({ errores: errores.array() });
+		return res.status(400).json({
+			errores: errores.array(),
+		});
+	}
+
+	try {
+		//Extraer el proyecto y comprobar si existe
+		const { proyecto } = req.body;
+		const existeProyecto = await Proyecto.findById(proyecto);
+		if (!existeProyecto) {
+			return res.status(404).json({
+				msg: "Proyecto no encontrado",
+			});
+		}
+
+		//Revisar si el proyecto actual pertenece al usuario autenticado
+		if (existeProyecto.creador.toString() !== req.usuario.id) {
+			return res.status(401).json({
+				msg: "No Autorizado",
+			});
+		}
+
+		//Creamos la tarea
+		const tarea = new Tarea(req.body);
+		await tarea.save();
+
+		res.json({
+			tarea,
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(500).send("Hubo un error");
+	}
+};
+
+//Obtiene las tareas por proyecto
+exports.obtenerTareas = async (req, res) => {
+	try {
+		//Extraer el proyecto y comprobar si existe
+		const { proyecto } = req.body;
+
+		const existeProyecto = await Proyecto.findById(proyecto);
+		if (!existeProyecto) {
+			return res.status(404).json({
+				msg: "Proyecto no encontrado",
+			});
+		}
+
+		//Revisar si el proyecto actual pertenece al usuario autenticado
+		if (existeProyecto.creador.toString() !== req.usuario.id) {
+			return res.status(401).json({
+				msg: "No Autorizado",
+			});
+		}
+
+		//Obtener las tareas por proyecto
+		const tareas = await Tarea.find({
+			proyecto,
+		});
+		res.json({
+			tareas,
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({
+			msg: "Hubo un error",
+		});
+	}
+};
+
+//Actualizar tareas
+exports.actualizarTarea = async (req, res) => {
+	try {
+		//Extraer el proyecto y comprobar si existe
+		const { proyecto, nombre, estado } = req.body;
+
+		//Si la tarea existe o no
+		let tarea = await Tarea.findById(req.params.id);
+
+		if (!tarea) {
+			return res.status(404).json({
+				msg: "No existe esa tarea",
+			});
+		}
+
+		//extraer proyecto
+		const existeProyecto = await Proyecto.findById(proyecto);
+
+		//Revisar si el proyecto actual pertenece al usuario autenticado
+		if (existeProyecto.creador.toString() !== req.usuario.id) {
+			return res.status(401).json({
+				msg: "No Autorizado",
+			});
+		}
+
+		//Crear un objeto con la nueva información
+		const nuevaTarea = {};
+		if (nombre) nuevaTarea.nombre = nombre;
+		if (estado) nuevaTarea.estado = estado;
+
+		//Guardar tarea
+		tarea = await Tarea.findOneAndUpdate({ _id: req.params.id }, nuevaTarea, { new: true });
+		res.json({ tarea });
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({
+			msg: "Hubo un error",
+		});
 	}
 };
--- MERNTasks/servidor/routes/tareas.js
+++ MERNTasks/servidor/routes/tareas.js
@@ -10,7 +10,12 @@ router.post(
 	"/",
 	auth,
 	[check("nombre", "El nombre es obligatorio").not().isEmpty()],
+	[check("proyecto", "El proyecto es obligatorio").not().isEmpty()],
 	tareaController.crearTarea
 );
+
+router.get("/", auth, tareaController.obtenerTareas);
+
+router.put("/:id", auth, tareaController.actualizarTarea);
 
 module.exports = router;
