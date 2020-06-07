bc83fb38a47827571b974cda6037a2bb7896b285
SoyDiego <diegofranchina@gmail.com>
2020-06-07 03:54:23 +0200

    Update port

--- MERNTasks/servidor/index.js
+++ MERNTasks/servidor/index.js
@@ -16,7 +16,7 @@ app.use(cors())
 app.use(express.json({ extended: true }));
 
 //Puerto de la app
-const port = process.env.port || 4000;
+const port = process.env.PORT || 4000;
 
 //Importar rutas
 app.use("/api/usuarios", require("./routes/usuarios.js"));
