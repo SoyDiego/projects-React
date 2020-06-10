ea5added3995c9335f443ad10a7a3c4823e61c7e
SoyDiego <diegofranchina@gmail.com>
2020-06-10 03:58:14 +0200

    Configured Redux

--- crudredux/src/App.jsx
+++ crudredux/src/App.jsx
@@ -4,19 +4,33 @@ import Productos from "./components/Productos";
 import NuevoProducto from "./components/NuevoProducto";
 import EditarProducto from "./components/EditarProducto";
 
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+// Redux
+import { Provider } from "react-redux";
+import store from "./store";
 
 function App() {
 	return (
 		<Router>
-			<Header />
-      <div className="container">
-        <Switch>
-          <Route exact path="/" component={Productos}/>
-          <Route exact path="/productos/nuevo" component={NuevoProducto}/>
-          <Route exact path="/productos/editar/:id" component={EditarProducto}/>
-        </Switch>
-      </div>
+			<Provider store={store}>
+				<Header />
+				<div className="container">
+					<Switch>
+						<Route exact path="/" component={Productos} />
+						<Route
+							exact
+							path="/productos/nuevo"
+							component={NuevoProducto}
+						/>
+						<Route
+							exact
+							path="/productos/editar/:id"
+							component={EditarProducto}
+						/>
+					</Switch>
+				</div>
+			</Provider>
 		</Router>
 	);
 }
---
+++ crudredux/src/reducers/productosReducer.jsx
@@ -0,0 +1,14 @@
+//Cada reducer tiene su propio state
+
+const initialState = {
+    productos: [],
+    error: null,
+    loading: false
+}
+
+export default function(state = initialState, action){
+    switch(action.type){
+        default:
+            return state;
+    }
+}
