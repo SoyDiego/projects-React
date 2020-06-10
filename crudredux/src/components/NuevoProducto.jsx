7f9a8b9a36dc27d89f350a1837740b5a95822356
SoyDiego <diegofranchina@gmail.com>
2020-06-10 02:05:49 +0200

    Form Add Product and Edit Product done

--- crudredux/src/components/NuevoProducto.jsx
+++ crudredux/src/components/NuevoProducto.jsx
@@ -1,7 +1,47 @@
-import React from 'react'
+import React from "react";
 
 const NuevoProducto = () => {
-    return <h1>Desde Nuevo Producto</h1>;
-}
+	return (
+		<div className="row justify-content-center mt-5">
+			<div className="col-md-8">
+				<div className="card">
+					<div className="card-body">
+						<h2 className="text-center mb-4 font-weight-bold">
+							Agregar Nuevo Producto
+						</h2>
 
-export default NuevoProducto
+						<form>
+							<div className="form-group">
+								<label>Nombre Producto</label>
+								<input
+									type="text"
+									className="form-control"
+									placeholder="Nombre Producto"
+									name="nombre"
+								/>
+							</div>
+
+							<div className="form-group">
+								<label>Precio Producto</label>
+								<input
+									type="number"
+									className="form-control"
+									placeholder="Precio Producto"
+									name="precio"
+								/>
+							</div>
+
+							<button
+								type="submit"
+								className="btn btn-primary font-weight-bold text-uppercase d-block w-100">
+								Agregar
+							</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default NuevoProducto;
