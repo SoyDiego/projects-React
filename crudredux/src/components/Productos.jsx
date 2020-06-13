3b348ee9a2e801128fddea0418036dc016c29269
SoyDiego <diegofranchina@gmail.com>
2020-06-13 22:27:30 +0200

    Added loading and show error

--- crudredux/src/components/Productos.jsx
+++ crudredux/src/components/Productos.jsx
@@ -13,14 +13,19 @@ const Productos = () => {
        // Consultar la API
        const cargarProductos = () => dispatch( obtenerProductosAction() )
        cargarProductos()
-    }, [])
+    }, [dispatch])
 
     //Obtener el state
     const productos = useSelector( state => state.productos.productos)
+    const error = useSelector(state => state.productos.error)
+    const cargando = useSelector(state => state.productos.loading)
 
     return (
         <Fragment>
             <h2 className="text-center my-5">Listado de Productos</h2>
+
+            {error && <p className="font-weight-bold alert alert-danger text-center mt-4">Hubo un error</p> }
+            {cargando && <p className="text-center">Cargando...</p>}
 
             <table className="table table-striped">
                 <thead className="bg-primary table-dark">
