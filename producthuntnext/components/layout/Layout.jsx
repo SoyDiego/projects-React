5317ce15c6710b98dc767335dd4860398ee61ef7
SoyDiego <diegofranchina@gmail.com>
2020-06-21 23:54:44 +0200

    Added Routing

--- producthuntnext/components/layout/Layout.jsx
+++ producthuntnext/components/layout/Layout.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
 
 const Layout = (props) => {
-    return (
-        <>
-        <h1>Header</h1>
-        <main>
-            {props.children}
-        </main>
-        </>
-    )
-}
+	return (
+		<>
+			<h1>Header</h1>
 
-export default Layout
+			<nav>
+				<Link href="/">Inicio</Link>
+				<Link href="/nosotros">Nosotros</Link>
+			</nav>
+			<main>{props.children}</main>
+		</>
+	);
+};
+
+export default Layout;
