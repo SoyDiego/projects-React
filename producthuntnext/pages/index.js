73c0a1932e3d501422d84c5c640f454a1a179790
SoyDiego <diegofranchina@gmail.com>
2020-06-21 23:37:19 +0200

    Starting project, adding Emotion & Babel

---
+++ producthuntnext/pages/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Head from 'next/head'
+import styled from "@emotion/styled"
+
+
+const Heading = styled.h1`
+  color: red;
+`
+const Home = () => {
+  return (
+    <div>
+      <Heading>Inicio</Heading>
+    </div>
+  )
+}
+
+export default Home
+
+
