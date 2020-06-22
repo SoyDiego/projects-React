93a3ef37172efc1d6cc2ba804c1ec787e05eac29
SoyDiego <diegofranchina@gmail.com>
2020-06-22 10:12:29 +0200

    Adding Global CSS

--- producthuntnext/components/layout/Layout.jsx
+++ producthuntnext/components/layout/Layout.jsx
@@ -1,11 +1,51 @@
 import React from "react";
 import Link from "next/link";
 import Header from "./Header"
+import {Global, css} from '@emotion/core'
 
 const Layout = (props) => {
 	return (
 		<>
-            <Header/>
+			<Global
+				styles={css`
+					:root {
+						--gris: #3d3d3d;
+						--gris2: #6f6f6f;
+						--naranja: #da552f;
+					}
+
+					html {
+						font-size: 62.5%;
+						box-sizing: border-box;
+					}
+
+					*,
+					*:before,
+					*:after {
+						box-sizing: inherit;
+					}
+					body {
+						font-size: 1.6rem;
+						line-height: 1.5;
+					}
+					h1,
+					h2,
+					h3 {
+						margin: 0 0 2rem 0;
+						line-height: 1.5;
+					}
+					ul{
+						list-style: none;
+						margin: 0;
+						padding: 0;
+					}
+
+					a{
+						text-decoration: none;
+					}
+				`}
+			/>
+			<Header />
 			<main>{props.children}</main>
 		</>
 	);
