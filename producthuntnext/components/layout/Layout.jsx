7c46407d5119708b9c8c1fe49f7b339bf13e9917
SoyDiego <diegofranchina@gmail.com>
2020-06-22 11:00:19 +0200

    Added normalize, fonts, etc.. and CSS in Header

--- producthuntnext/components/layout/Layout.jsx
+++ producthuntnext/components/layout/Layout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Link from "next/link";
-import Header from "./Header"
-import {Global, css} from '@emotion/core'
+import Header from "./Header";
+import { Global, css } from "@emotion/core";
+import Head from "next/head";
 
 const Layout = (props) => {
 	return (
@@ -11,6 +11,7 @@ const Layout = (props) => {
 					:root {
 						--gris: #3d3d3d;
 						--gris2: #6f6f6f;
+						--gris3: #e1e1e1;
 						--naranja: #da552f;
 					}
 
@@ -27,6 +28,7 @@ const Layout = (props) => {
 					body {
 						font-size: 1.6rem;
 						line-height: 1.5;
+						font-family: "PT Sans", sans-serif;
 					}
 					h1,
 					h2,
@@ -34,17 +36,44 @@ const Layout = (props) => {
 						margin: 0 0 2rem 0;
 						line-height: 1.5;
 					}
-					ul{
+
+					h1,
+					h2 {
+						font-family: "Roboto Slab", serif;
+						font-weight: 700;
+					}
+
+					h3 {
+						font-family: "PT Sans", sans-serif;
+					}
+					ul {
 						list-style: none;
 						margin: 0;
 						padding: 0;
 					}
 
-					a{
+					a {
 						text-decoration: none;
 					}
 				`}
 			/>
+
+			<Head>
+				<html lang="es" />
+				<title>Product Hunt Firebase & Next.js</title>
+				<link
+					rel="stylesheet"
+					href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+					integrity="sha256-l85OmPOjvil/SOvVt3HnSSjzF1TUMyT9eV0c2BzEGzU="
+					crossorigin="anonymous"
+				/>
+				<link
+					href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Roboto+Slab:wght@400;700&display=swap"
+					rel="stylesheet"
+				/>
+				<link href="/static/css/app.css" rel="stylesheet" />
+			</Head>
+
 			<Header />
 			<main>{props.children}</main>
 		</>
