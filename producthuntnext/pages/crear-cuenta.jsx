83e22dc469fb14d5580f91573620df092ddf01e5
SoyDiego <diegofranchina@gmail.com>
2020-06-23 11:07:11 +0200

    Show Errors in Form CrearCuenta done

--- producthuntnext/pages/crear-cuenta.jsx
+++ producthuntnext/pages/crear-cuenta.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { css } from "@emotion/core";
 import Layout from "../components/layout/Layout";
-import { Formulario, Campo, InputSubmit } from "../components/ui/Formulario";
+import { Formulario, Campo, InputSubmit, Error } from "../components/ui/Formulario";
 
 //Validaciones
 import useValidacion from "../hooks/useValidacion";
@@ -17,9 +17,9 @@ const CrearCuenta = () => {
 	const {
 		valores,
 		errores,
-		submitForm,
 		handleSubmit,
 		handleChange,
+		handleBlur
 	} = useValidacion(STATE_INICIAL, validarCrearCuenta, crearCuenta);
 
 	const { nombre, email, password } = valores;
@@ -49,8 +49,11 @@ const CrearCuenta = () => {
 								name="nombre"
 								value={nombre}
 								onChange={handleChange}
+								onBlur={handleBlur}
 							/>
 						</Campo>
+
+						{errores.nombre && <Error>{errores.nombre}</Error>}
 
 						<Campo>
 							<label htmlFor="nombre">Email</label>
@@ -61,8 +64,11 @@ const CrearCuenta = () => {
 								name="email"
 								value={email}
 								onChange={handleChange}
+								onBlur={handleBlur}
 							/>
 						</Campo>
+
+						{errores.email && <Error>{errores.email}</Error>}
 
 						<Campo>
 							<label htmlFor="password">Password</label>
@@ -73,8 +79,11 @@ const CrearCuenta = () => {
 								name="password"
 								value={password}
 								onChange={handleChange}
+								onBlur={handleBlur}
 							/>
 						</Campo>
+
+						{errores.password && <Error>{errores.password}</Error>}
 
 						<InputSubmit type="submit" value="Crear Cuenta" />
 					</Formulario>
