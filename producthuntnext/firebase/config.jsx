8ce419491dde2db3244e367e00a75196257f0f29
SoyDiego <diegofranchina@gmail.com>
2020-06-24 19:28:15 +0200

    Configured and Installed firebase done

---
+++ producthuntnext/firebase/config.jsx
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: "AIzaSyDpkR3u6eY49UqC25GCm2mH9l8AVATQHd0",
+	authDomain: "product-hunt-fe387.firebaseapp.com",
+	databaseURL: "https://product-hunt-fe387.firebaseio.com",
+	projectId: "product-hunt-fe387",
+	storageBucket: "product-hunt-fe387.appspot.com",
+	messagingSenderId: "949322960662",
+	appId: "1:949322960662:web:89ddf8ab3f41a7d0328a37",
+	measurementId: "G-M302LXHRZQ",
+};
+
+export default firebaseConfig;
