4130854fe3d8fef936646b23f7f8388555cef45d
SoyDiego <diegofranchina@gmail.com>
2020-06-25 10:51:11 +0200

    Created Form Login and login done

--- producthuntnext/pages/login.jsx
+++ producthuntnext/pages/login.jsx
@@ -1,11 +1,99 @@
-import React from "react";
+import React, { useState } from "react";
+import { css } from "@emotion/core";
+import Router from "next/router";
 import Layout from "../components/layout/Layout";
+import {
+	Formulario,
+	Campo,
+	InputSubmit,
+	Error,
+} from "../components/ui/Formulario";
+
+import firebase from "../firebase";
+
+//Validaciones
+import useValidacion from "../hooks/useValidacion";
+import validarCrearCuenta from "../validacion/validarIniciarSesion";
+import validarIniciarSesion from "../validacion/validarIniciarSesion";
+
+const STATE_INICIAL = {
+	email: "",
+	password: "",
+};
 
 const Login = () => {
+	const [error, guardarError] = useState(false);
+
+	const {
+		valores,
+		errores,
+		handleSubmit,
+		handleChange,
+		handleBlur,
+	} = useValidacion(STATE_INICIAL, validarIniciarSesion, iniciarSesion);
+
+	const { email, password } = valores;
+
+	async function iniciarSesion() {
+		try {
+			await firebase.login(email, password);
+			Router.push("/");
+		} catch (error) {
+			console.error(
+				"Hubo un error al autenticar el usuario",
+				error.message
+			);
+			guardarError(error.message);
+		}
+	}
+
 	return (
 		<div>
 			<Layout>
-				<h1>Login</h1>
+				<>
+					<h1
+						css={css`
+							text-align: center;
+							margin-top: 5rem;
+						`}>
+						Iniciar Sesión
+					</h1>
+					<Formulario onSubmit={handleSubmit} noValidate>
+						<Campo>
+							<label htmlFor="nombre">Email</label>
+							<input
+								type="text"
+								id="email"
+								placeholder="Tu Email"
+								name="email"
+								value={email}
+								onChange={handleChange}
+								onBlur={handleBlur}
+							/>
+						</Campo>
+
+						{errores.email && <Error>{errores.email}</Error>}
+
+						<Campo>
+							<label htmlFor="password">Password</label>
+							<input
+								type="text"
+								id="password"
+								placeholder="Tu Password"
+								name="password"
+								value={password}
+								onChange={handleChange}
+								onBlur={handleBlur}
+							/>
+						</Campo>
+
+						{errores.password && <Error>{errores.password}</Error>}
+
+						{error && <Error>{error}</Error>}
+
+						<InputSubmit type="submit" value="Iniciar Sesión" />
+					</Formulario>
+				</>
 			</Layout>
 		</div>
 	);
