d77e73eaa6ce627573dab310ceaf2eeadcbf6e99
SoyDiego <diegofranchina@gmail.com>
2020-06-29 11:32:48 +0200

    Search Products done

--- producthuntnext/components/ui/Buscar.jsx
+++ producthuntnext/components/ui/Buscar.jsx
@@ -1,43 +1,63 @@
-import React from 'react'
-import styled from '@emotion/styled'
-import {css} from '@emotion/core'
+import React, { useState } from "react";
+import styled from "@emotion/styled";
+import { css } from "@emotion/core";
+import Router from 'next/router'
 
 const InputText = styled.input`
-    border: 1px solid var(--gris3);
-    padding: 1rem;
-    min-width: 300px;
-`
+	border: 1px solid var(--gris3);
+	padding: 1rem;
+	min-width: 300px;
+`;
 
 const InputSubmit = styled.button`
-    height: 3rem;
-    width: 3rem;
-    display: block;
-    background-size: 4rem;
-    background-image: url('/static/img/buscar.png');
-    background-repeat: no-repeat;
-    position: absolute;
-    right: 1rem;
-    top: 1px;
-    background-color: white;
-    border: none;
-    text-indent: -9999px;
+	height: 3rem;
+	width: 3rem;
+	display: block;
+	background-size: 4rem;
+	background-image: url("/static/img/buscar.png");
+	background-repeat: no-repeat;
+	position: absolute;
+	right: 1rem;
+	top: 1px;
+	background-color: white;
+	border: none;
+	text-indent: -9999px;
 
-    &:hover{
-        cursor: pointer;
-    }
-
-`
+	&:hover {
+		cursor: pointer;
+	}
+`;
 
 const Buscar = () => {
-    return (
-        <form css={css`
-            position: relative;
-        `}>
-            <InputText  type="text" placeholder="Buscar Productos"/>
+	const [busqueda, guardarBusqueda] = useState("");
 
-            <InputSubmit type="submit">Buscar</InputSubmit>
-        </form>
-    )
-}
+	const buscarProducto = (e) => {
+        e.preventDefault();
+        
+        if (busqueda.trim() === "") return;
+        
+        //Redireccionar al usuario
+        Router.push({
+            pathname: '/buscar',
+            query: {q : busqueda}
+        })
+	};
 
-export default Buscar
+	return (
+		<form
+			css={css`
+				position: relative;
+			`}
+			onSubmit={buscarProducto}>
+			<InputText
+				type="text"
+				placeholder="Buscar Productos"
+				onChange={(e) => guardarBusqueda(e.target.value)}
+			/>
+
+			<InputSubmit type="submit">Buscar</InputSubmit>
+		</form>
+	);
+};
+
+export default Buscar;
