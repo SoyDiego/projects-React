a35781975d6e498271725de97102ed47421c92df
SoyDiego <diegofranchina@gmail.com>
2020-06-30 12:57:28 +0200

    Added Helmet, Normalize, google fonts and Global CSS

--- hotelgatsby/src/components/layout.jsx
+++ hotelgatsby/src/components/layout.jsx
@@ -1,10 +1,53 @@
 import React from "react"
-import Header from "../components/header"
+import Helmet from "react-helmet"
+import { Global, css } from "@emotion/core"
+import Header from "./header"
 
 const Layout = props => {
   return (
     <div>
       <>
+        <Global
+          styles={css`
+            html {
+              font-size: 62.5%;
+            }
+            body {
+              font-size: 1.6rem;
+              line-height: 1.5;
+            }
+            h1,
+            h2,
+            h3 {
+              margin: 0;
+              line-height: 1.5;
+            }
+
+            h1,
+            h2 {
+              font-family: "Roboto", serif;
+            }
+            h3 {
+              font-family: "PT Sans", sans-serif;
+            }
+            ul {
+              list-style: none;
+              margin: 0;
+              padding: 0;
+            }
+          `}
+        />
+        <Helmet>
+          <title>Gatsby Hotel</title>
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Roboto:wght@400;700&display=swap"
+            rel="stylesheet"
+          />
+        </Helmet>
         <Header />
         {props.children}
       </>
