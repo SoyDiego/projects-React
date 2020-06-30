bf755ad83158e4c9d3822d387f4b63a3829dfe03
SoyDiego <diegofranchina@gmail.com>
2020-06-30 13:48:14 +0200

    Added Header and Nav

--- hotelgatsby/src/components/header.jsx
+++ hotelgatsby/src/components/header.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React from "react"
+import { css } from "@emotion/core"
+import Navegacion from "./nav"
 
 const Header = () => {
-    return (
-        <div>
-            <h1>Desde Header</h1>
-        </div>
-    )
+  return (
+    <header
+      css={css`
+        background-color: #333;
+        padding: 1rem;
+      `}
+    >
+      <div
+        css={css`
+          max-width: 1200px;
+          margin: 0 auto;
+
+          @media (min-width: 768px) {
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+          }
+        `}
+      >
+        <h1
+          css={css`
+            color: #ffffff;
+            text-align: center;
+          `}
+        >
+          Hotel Gatsby
+        </h1>
+
+        <Navegacion />
+      </div>
+    </header>
+  )
 }
 
 export default Header
--- hotelgatsby/src/pages/index.jsx
+++ hotelgatsby/src/pages/index.jsx
@@ -3,7 +3,7 @@ import Layout from "../components/layout"
 
 const IndexPage = () => (
   <Layout>
-    <h1>Gatsby Hotel</h1>
+    <h1>Index</h1>
   </Layout>
 )
 
