f015141506f7723fcf3c1ea9080d3fa940f2a8a8
SoyDiego <diegofranchina@gmail.com>
2020-07-01 14:04:20 +0200

    Added CSS in ContenidoInicio

--- hotelgatsby/src/components/contenidoInicio.jsx
+++ hotelgatsby/src/components/contenidoInicio.jsx
@@ -1,5 +1,25 @@
 import React from "react"
 import { graphql, useStaticQuery } from "gatsby"
+import Image from "gatsby-image"
+import styled from "@emotion/styled"
+import { css } from "@emotion/core"
+
+const TextoInicio = styled.div`
+  padding-top: 4rem;
+  max-width: 1200px;
+  width: 95%;
+  margin: 0 auto;
+
+  @media (min-width: 768px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+  }
+
+  p {
+    line-height: 2;
+  }
+`
 
 const ContenidoInicio = () => {
   const informacion = useStaticQuery(graphql`
@@ -24,12 +44,20 @@ const ContenidoInicio = () => {
 
   return (
     <>
-      <h2>{titulo}</h2>
+      <h2
+        css={css`
+          text-align: center;
+          font-size: 4rem;
+          margin-top: 4rem;
+        `}
+      >
+        {titulo}
+      </h2>
 
-      <div>
+      <TextoInicio>
         <p>{contenido}</p>
-        <img src={imagen.fluid.src} alt="Imagen hotel" />
-      </div>
+        <Image fluid={imagen.fluid} alt="Imagen hotel" />
+      </TextoInicio>
     </>
   )
 }
