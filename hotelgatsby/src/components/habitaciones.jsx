dad56506e5377d326e8399ebf6873292205143e7
SoyDiego <diegofranchina@gmail.com>
2020-07-02 20:59:33 +0200

    Generating dynamic pages

---
+++ hotelgatsby/src/components/habitaciones.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HabitacionTemplate = () => {
+    return (
+        <div>
+            <h1>Habitacion</h1>
+        </div>
+    )
+}
+
+export default HabitacionTemplate
